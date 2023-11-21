1512c42cef1bbfc62aac3e7c028afb19cb4fc29a
Marquinhos22 <marcos.ramos.carp@gmail.com>
2023-11-21 20:37:06 -0300

    Mission 3 finalizada

--- js/item.js
+++ js/item.js
@@ -3,7 +3,5 @@ const subtract = document.querySelector('#subtract');
 const quantity = document.querySelector('#quantity');
 
 add.addEventListener('click', () => quantity.value= Number(quantity.value) + 1)
-subtract.addEventListener('click', () => quantity.value= Number(quantity.value) - 1);
 
-
-console.log(add, subtract, quantity);
+subtract.addEventListener('click', () => { if (quantity.value>0) { quantity.value= Number(quantity.value) - 1 }});
