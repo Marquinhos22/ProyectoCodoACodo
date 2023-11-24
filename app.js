ed8001567d53d24059288799dca5d50b738d6f9a
Marquinhos22 <marcos.ramos.carp@gmail.com>
2023-11-23 23:51:53 -0300

    Primera configuracion del servidor estatico

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+app.listen(PORT, () => {
+    console.log(`El servidor esta corriendo el puerto 3000  http://localhost:${PORT}` )});
