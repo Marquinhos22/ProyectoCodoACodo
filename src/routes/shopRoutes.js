9b609ef90a514bd16ed691ac342107d04300fa02
Marquinhos22 <68550774+Marquinhos22@users.noreply.github.com>
2023-11-29 12:10:34 -0300

    Update shopRoutes.js

--- src/routes/shopRoutes.js
+++ src/routes/shopRoutes.js
@@ -1,27 +1,18 @@
 const express = require('express');
+const shopControllers = require('../controllers/shopControllers');
 
 //necesito el metodo router() de express
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Esta ruta devuelve la vista de Shop');
-})
+router.get('/', shopControllers.shop)
 
-router.get('/item/:id', (req, res) => {
-    res.send(`Esta ruta devuelve el ID: ${req.params.id} del producto funko`);
-})
+router.get('/item/:id', shopControllers.itemID )
 
-router.post('/item/:id/add', (req, res) => {
-    res.send(`Esta ruta agrega el ID: ${req.params.id} del producto funko `);
-})
+router.post('/item/:id/add', shopControllers.itemIDAdd )
 
-router.get('/cart', (req, res) => {
-    res.send('Esta ruta devuelve la vista de Carrito');
-})
+router.get('/cart', shopControllers.cartG)
 
-router.post('/cart', (req, res) => {
-    res.send('Esta ruta envia los datos de Carrito');
-})
+router.post('/cart', shopControllers.cartP)
 
 
 module.exports = router;
